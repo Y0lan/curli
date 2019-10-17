3a707a841d9e36628a56c5c089fbf3069a09aa15
yolan <bkV97"V-ob}aGJ\R-B}L98sG4dsJ#Fy58'K:4*_Ft+4-jRaMK%~}pk)6X89U%>
2019-10-17 16:57:20 +0200

    .

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <time.h>
+#include <unistd.h>
 #include "main.h"
 char * now(){
     time_t now;
@@ -7,6 +8,10 @@ char * now(){
     return ctime(&now);
 }
 int main(int argc, char ** argv){
-    printf("%s",now());
+    daemon(0,0);
+    while (1){
+        sleep(200);
+        break;
+    }
     return 0;
 }
